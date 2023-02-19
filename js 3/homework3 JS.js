57d1974086c4b95c190bd3895def3d92fac992be
eugenPipen <eugenesalnickov@yandex.ru>
2023-02-19 12:51:15 +0800

    создвал папку и файл в мастере

---
+++ js 3/homework3 JS.js
@@ -0,0 +1,17 @@
+let height= prompt('how tall are you?');
+if (height>160){
+    alert('you can go to the attraction')
+}else if (height<160){
+    alert('youll grow up and take a ride');  
+} else{
+    alert('you cant go to the attraction');
+}
+
+let weight= prompt('how much do you weigh?');
+if (weight>60){
+    alert('you can go to the attraction')
+}else if (weight<60){
+    alert('Youre too light'); 
+} else{
+    alert('you cant go to the attraction');
+}
