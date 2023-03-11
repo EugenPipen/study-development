70a93c90fa94a6d5c46405ca06cbb4f3f2f32b39
eugenPipen <eugenesalnickov@yandex.ru>
2023-03-11 11:29:56 +0800

    прописал алерт

--- homework 6 massiv/massiv.js
+++ homework 6 massiv/massiv.js
@@ -24,11 +24,14 @@ let Caesar={
 };
 //Массив из обьектов
 let menu = [Soup,Pizza,Caesar];
+alert(`Созданный массив из объектов для меню: ${JSON.stringify(menu)}`);
 //Удалить один обьект из массива
 menu.splice(1,1);
+alert(`Удалили одно блюдо из меню: ${JSON.stringify(menu)}`);
 //Добавить этот блюдо обратно на первое место, но уберать один из ингредиентов
 delete Soup.ingredients.potato;
 menu.unshift(Soup);
+alert(`Добавили обратно блюдо, которое удалили но без одного ингредиента: ${JSON.stringify(menu)}`);
 //указать закупочную цену и цену продажи
 menu.forEach(function (element) {
     switch  (element.dish ) {
@@ -44,11 +47,16 @@ menu.forEach(function (element) {
             element.price = 200;
             element.purchasePrice = 300;
             break;
+            default:
+                alert("Error");
         }
     });
+    alert(`Добавили каджому блюду в меню по новым параметрам(цена и время приготовления): ${JSON.stringify(menu)}`);
 //Посчитать прибыль с каждого блюда
 let sum =  Soup.price-Soup.purchasePrice+Pizza.price-Pizza.purchasePrice+Caesar.price-Caesar.purchasePrice
+alert(`Сумма заказа: ${sum} $`);
 //удалить все элементы из массива кроме одного
 menu.splice(1);
+alert(`Меню после удаления всех элементов корме одного: ${JSON.stringify(menu)}`);
 
 
