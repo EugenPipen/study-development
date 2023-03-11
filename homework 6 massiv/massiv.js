5bba61c483a6a14de5146603f353d048aac1667b
eugenPipen <eugenesalnickov@yandex.ru>
2023-03-11 11:23:59 +0800

    переделал ошибку в словах

--- homework 6 massiv/massiv.js
+++ homework 6 massiv/massiv.js
@@ -2,34 +2,33 @@
  
 //Несколько обьектов и описание
 let Soup={
-
+ dish:"Суп",
 ingredients:{
-    dish:"Суп",
     water:100,
     salt:10,
     potato:30,},
 };
 let Pizza={
-    ingredients:{
     dish:"Пицца",
+    ingredients:{
     dough:30,
     tomato:50,
     cheese:40},
 };
 let Caesar={
-    Dish:"Цезарь",
+    dish:"Цезарь",
         ingredients:{
         crackers:10,
         tomato:30,
         feta:50},
 };
 //Массив из обьектов
-let menu = [soup,pizza,caesar];
+let menu = [Soup,Pizza,Caesar];
 //Удалить один обьект из массива
 menu.splice(1,1);
 //Добавить этот блюдо обратно на первое место, но уберать один из ингредиентов
-delete soup.ingredients.potato;
-menu.unshift(soup);
+delete Soup.ingredients.potato;
+menu.unshift(Soup);
 //указать закупочную цену и цену продажи
 menu.forEach(function (element) {
     switch  (element.dish ) {
@@ -48,7 +47,7 @@ menu.forEach(function (element) {
         }
     });
 //Посчитать прибыль с каждого блюда
-let sum =  Soup.price-soup.purchasePrice+Pizza.price-Pizza.purchasePrice+Caesar.price-Caesar.purchasePrice
+let sum =  Soup.price-Soup.purchasePrice+Pizza.price-Pizza.purchasePrice+Caesar.price-Caesar.purchasePrice
 //удалить все элементы из массива кроме одного
 
 
